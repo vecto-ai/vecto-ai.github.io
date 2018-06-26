7dd29733f3db6f8f40fd0f620bc44615b2138ade
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-06-26 16:46:18 +0900

    mainained footer

--- conf.py
+++ conf.py
@@ -899,7 +899,7 @@ LICENSE = ""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
-CONTENT_FOOTER = 'Contents &copy; {date}         <a href="mailto:{email}">{author}</a>         {license}'
+CONTENT_FOOTER = 'Maintained by <a href="mailto:{email}">{author}</a>         {license}'
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
